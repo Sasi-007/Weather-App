ac2fe46c4954ead3aa673940283d20bfc717ad64
Arianne Ghislaine Rull <arianneghislaine@gmail.com>
2019-06-16 22:01:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 window.addEventListener('load' ()=> {
   let long;
   let lat;
-
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
+
+      const proxy ="https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/542f87346de4ffd5d5d143cdfd31482e/${lat},${long}`;
+
+      // get request //
+      fetch(api)
+        .then(response =>{
+          return response.json();
+        })
+        .then(data =>{
+          console.log(data);
+        })
     });
   }
 });
