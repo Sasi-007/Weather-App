98278293d9bd3470b265d7966362942aec0fbdc0
Arianne Ghislaine Rull <arianneghislaine@gmail.com>
2019-06-29 20:31:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,49 @@
-window.addEventListener('load' ()=> {
-  let long;
-  let lat;
+$(document).ready(function(){
+  var lat;
+  var long;
 
-  //defining the variables//
-  const temperatureDescription = document.querySelector('.temperature-description');
-  const temperatureDegree = document.querySelector('.temperature-degree');
-  const temperatureTimezone = document.querySelector('.temperature-timezone');
-  const temperatureSelection = document.querySelector('.temperature')
-  ;
-  const temperatureSpan = document.querySelector('temperature span');
-
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position => {
-      long = position.coords.longitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position)
       lat = position.coords.latitude;
+      long = position.coords.longitude;
 
-      const proxy ="https://cors-anywhere.herokuapp.com/";
-      const api = `${proxy}https://api.darksky.net/forecast/542f87346de4ffd5d5d143cdfd31482e/${lat},${long}`;
+      var api = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long + '';
 
-      // get request //
-      fetch(api)
-        .then(response =>{
-          return response.json();
-        })
-        .then(data =>{
-          console.log(data);
-          const { temperature, summary, icon } = data.currently;
-          //Set DOM Elements from the API//
-          temperatureDegree.textContent = temperature;
-          temperatureDescription.textContent = summary;
-          locationTimezone.textContent = data.timezone;
-            //Formula for Celsius//
-            let  celsius = (temperature - 32) * (5 / 9);
-            //Set Icon//
-            setIcons(icon, document.querySelector('.icon'))
-            //Change temperature to Celsius/Farenheit//
-              temperatureSection.addEventListener('click', () =>{
-                if(temperatureSpan.textContent === "F") {
-                  temperatureSpan.textContent = "C";
-                  //Showing the conversion//
-                  temperatureDegree.textContent = Math.floor(celsius);
-                } else {
-                  temperatureSpan.textContent = "F";
-                  temperatureDegree.textContent = temperature;
-                }
-              });
-        })
-    });
-  }
+      $.getJSON(api, function (res){
+        var celsius = res.main.temp;
+        var farenheit = (celsius * 1.8) + 32;
+        var location = res.name;
 
-  //define the function//
-  function (setIcon, iconID){
-    const skycons = new Skycons ({color: "white"});
-    //Finds the line to replace it with an underscore//
-    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-    skycons.play();
-    return skycons.set(iconID, Skycons[currentIcon]);
-  }
+        $('.weather-location').html(location);
+        $('.temp').html(Math.floor(celsius));
+        $('.weather-description').html(res.weather[0].description);
+        $('.weatherType').attr('id', res.weather[0].main);
+        $('.row2').on('click', function() {
+          if ($('.temp').html() == (Math.floor(celsius))) {
+            $('.temp').html(Math.floor(farenheit));
+            $('temp-type').html('°F');
+          } else{
+            $('.temp').html(Math.floor(celsius));
+            $('.temp-type').html('°C');
+          }
+        });
+
+        //setting up the icon
+        var icons = new Skycons({
+            "color": "white"
+        });
+
+        icons.set("Clear-day", Skycons.CLEAR_DAY);
+        icons.set("Clear-night", Skycons.CLEAR_NIGHT);
+        icons.set("Partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
+        icons.set("Partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
+        icons.set("Clouds", Skycons.CLOUDY);
+        icons.set("Rain", Skycons.RAIN);
+        icons.set("Sleet", Skycons.SLEET);
+        icons.set("Snow", Skycons.SNOW);
+        icons.set("Wind", Skycons.WIND);
+        icons.set("Fog", Skycons.FOG);
+        icons.play();
+      });
+  )};
 });
